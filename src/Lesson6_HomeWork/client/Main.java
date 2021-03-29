8fd418c73c0bd13e7c0c4feacc0932697114d119
IvanGer212 <Comradeivan88@gmail.com>
2021-03-29 14:54:03 +0300

    Create Thread for listening message from server, and add possibility to write message to server. Create Thread for listening from client, and add possibility to write message to client.

--- src/Lesson6_HomeWork/client/Main.java
+++ src/Lesson6_HomeWork/client/Main.java
@@ -1,4 +1,9 @@
 package Lesson6_HomeWork.client;
 
+import Lesson6_HomeWork.server.Server;
+
 public class Main {
+    public static void main(String[] args) {
+        new Server();
+    }
 }
--- src/Lesson6_HomeWork/server/Main.java
+++ src/Lesson6_HomeWork/server/Main.java
@@ -1,4 +1,9 @@
 package Lesson6_HomeWork.server;
 
+import Lesson6_HomeWork.client.Client;
+
 public class Main {
+    public static void main(String[] args) {
+        new Client();
+    }
 }
