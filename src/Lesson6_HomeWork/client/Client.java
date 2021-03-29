c3c84be5e66a68af595d803612dd587f1b3c734f
IvanGer212 <Comradeivan88@gmail.com>
2021-03-29 12:11:28 +0300

    Create new Sockets for client and for server with ip and port. Add in and out DataStream

--- src/Lesson6_HomeWork/client/Client.java
+++ src/Lesson6_HomeWork/client/Client.java
@@ -1,4 +1,25 @@
 package Lesson6_HomeWork.client;
 
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    Client(){
+        try {
+            Socket socket = new Socket("127.0.0.1",8080);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
--- src/Lesson6_HomeWork/server/Server.java
+++ src/Lesson6_HomeWork/server/Server.java
@@ -1,4 +1,18 @@
 package Lesson6_HomeWork.server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    Server(){
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            Socket accept = serverSocket.accept();
+            DataInputStream in = new DataInputStream(accept.getInputStream());
+            DataOutputStream out = new DataOutputStream(accept.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
