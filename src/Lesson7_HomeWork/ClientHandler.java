73b16c0d97102c52e8be2322994ab07b26204af2
IvanGer212 <Comradeivan88@gmail.com>
2021-04-04 17:35:29 +0300

    Add function for check Logged client or not. Add Function for add and remove user to set of logged users. Add method broadcast.

--- src/Lesson7_HomeWork/ClientHandler.java
+++ src/Lesson7_HomeWork/ClientHandler.java
@@ -10,9 +10,13 @@ public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    String name;
 
+    public String getName() {
+        return name;
+    }
 
-    public ClientHandler(Socket socket, AuthenticationService authenticationService) {
+    public ClientHandler(Socket socket, Server server) {
         this.socket = socket;
         try {
             in = new DataInputStream(socket.getInputStream());
@@ -21,11 +25,11 @@ public class ClientHandler {
             throw new ChatServerException("Something went wrong during client establishing",e);
         }
 
-        doAuthentication(authenticationService);
+        doAuthentication(server);
 
     }
 
-    private void doAuthentication(AuthenticationService authenticationService){
+    private void doAuthentication(Server server){
         while (true){
             try {
 
@@ -34,15 +38,43 @@ public class ClientHandler {
                  -auth l1 p1
                  */
                 if (msg.startsWith("-auth")){
-                    String[] credentials = msg.split("\\s");
-                    String login = credentials[1];
-                    String password = credentials[2];
-                    authenticationService.getEntryByCredentials(login, password)
+                    String[] credentialsStruct = msg.split("\\s");
+                    String login = credentialsStruct[1];
+                    String password = credentialsStruct[2];
+                    Optional<AuthenticationService.Entry> mayBeCredentials = server.getAuthenticationService()
+                            .getEntryByCredentials(login, password);
+
+                    if (mayBeCredentials.isPresent()){
+                        AuthenticationService.Entry credentials = mayBeCredentials.get();
+                        if (!server.isLoggedIn(credentials.getName())){
+                            name = credentials.getName();
+                            server.broadcast(String.format("User[%s] entered the chat", name));
+                            server.subscribe(this);
+                            break;
+                        } else{
+                            sendMessage(String.format("User with name %s is already logged",credentials.getName()));
+                        }
+                    }
+                    else {
+                        sendMessage("Incorrect login or password");
+                    }
                 }
             } catch (IOException e) {
                 throw new ChatServerException("Something went wrong during client authentication",e);
             }
 
         }
+    }
+
+    public void receivedMessage(){
+
+    }
+    public void sendMessage(String msg){
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            throw new ChatServerException("Something went wrong during sending a message.");
+        }
+
     }
 }
--- src/Lesson7_HomeWork/Server.java
+++ src/Lesson7_HomeWork/Server.java
@@ -3,18 +3,58 @@ package Lesson7_HomeWork;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server {
     private final AuthenticationService authenticationService;
+    private final Set<ClientHandler> loggedClient;
 
     public Server() {
         try {
             authenticationService = new AuthenticationService();
+            loggedClient = new HashSet<>();
             ServerSocket socket = new ServerSocket(8080);
             Socket clientSocked = socket.accept();
         } catch (IOException e) {
             throw new ChatServerException("Something went wrong", e);
         }
 
+    }
+
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
+    public void subscribe(ClientHandler clientHandler){
+        loggedClient.add(clientHandler);
+    }
+
+    public void unsubscribe (ClientHandler clientHandler){
+        loggedClient.remove(clientHandler);
+    }
+
+    public void broadcast(String msg){
+        for (ClientHandler clientHandler: loggedClient) {
+            clientHandler.sendMessage(msg);
+
+        }
+
+    }
+
+    public boolean isLoggedIn(String name){
+       return loggedClient.stream()
+                    .filter(client->client.getName().equals(name))
+                    .findFirst().isPresent();
+        /**Iterator<ClientHandler> iterator = loggedClient.iterator();
+        while (iterator.hasNext()){
+            ClientHandler client = iterator.next();
+            if (client.getName.equals(name)){
+                return true;
+            }
+        }
+        return false;
+    */
     }
 }
